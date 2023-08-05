86cccadae144462b745638bded5f9f9ae234a6f1
Godspower Itedjere <itedjereg77@gmail.com>
2023-08-05 15:38:30 +0100

    Created a new project on firebase and installed firebase into our frontend using npm and initialized our frontend app

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log( 'hello from index.js' ); 
+import { initializeApp } from 'firebase/app'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBp-SxTtmzgxgpE3T7VvT9vLxnwNPBRGZE",
+    authDomain: "fir-9-lessons-12847.firebaseapp.com",
+    projectId: "fir-9-lessons-12847",
+    storageBucket: "fir-9-lessons-12847.appspot.com",
+    messagingSenderId: "600069869007",
+    appId: "1:600069869007:web:5ded73986f61b023b956ba"
+};
+
+initializeApp( firebaseConfig );
